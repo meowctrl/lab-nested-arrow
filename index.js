bd6f9226cee548553ccf574870ad85c00eae6adf
carol <carolineallen1012@gmail.com>
2025-12-02 00:48:09 -0500

    login tracker created

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+const userInfo = {
+  'user': 
+  'password' ,
+}
 
-
-
+function createLoginTracker(userInfo){
+   let attemptCount = 0;
+   const loginAttempt = (passwordAttempt) => {
+    attemptCount++;
+    if(passwordAttempt === userInfo.password && attemptCount <= 3) { 
+      return 'Login successful';
+    } else if(passwordAttempt !== userInfo.password && attemptCount <= 3) { 
+      return `Attempt ${attemptCount}: Login failed`;
+      } else if(attemptCount > 3) { 
+      return 'Account locked due to too many failed login attempts';
+    }
+  }
+  return loginAttempt;
+}
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
 };
